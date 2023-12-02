959569a1f263de4f5465a06b8e8c88c856f79d1e
Aditi174 <070801ap@gmail.com>
2023-12-02 13:07:47 +0530

    final code

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ const user_input = document.getElementById("inputs")
 const Button = document.getElementById("buttons")
 const container = document.getElementById("item")
 
-const array = JSON.parse(localStorage.getItem('Key')) || [];
+let array = JSON.parse(localStorage.getItem('Key')) || [];
 
 Button.addEventListener("click",()=>{
     const inputvalue = user_input.value
@@ -23,7 +23,76 @@ function display(){
         listitems.className = "list";
         listitems.textContent = i;
 
+        let deleteicon = document.createElement("i");
+        deleteicon.className = "fa-solid fa-trash";
+        listitems.appendChild(deleteicon);
+
+        let editicon = document.createElement("span")
+        editicon.className = "material-symbols-outlined"; 
+        editicon.textContent = "edit";
+        listitems.appendChild(editicon);
+
+        
+        deleteicon.addEventListener('click', () => {
+            deleteitems(i);
+        });
+
+        editicon.addEventListener('click',()=>{
+            update(i)
+        })
+
         container.appendChild(listitems)
     }
+
+
+    
+}
+
+function deleteitems(i){
+    array = array.filter(ele => ele !==i)
+
+    localStorage.setItem('Key', JSON.stringify(array));
+    display()
+
+}
+
+function update(i){
+    const index = array.indexOf(i)
+
+    const model = document.createElement("div");
+    model.className = "overlay";
+    document.body.appendChild(model);
+
+    const main = document.createElement("div")
+    main.className = "Main"
+    document.body.appendChild(main)
+
+    const nextinput = document.createElement("input")
+    nextinput.className = "N_input"
+    nextinput.type = "text"
+    nextinput.value = i
+
+    main.appendChild(nextinput)
+
+    const savebutton = document.createElement("button");
+    savebutton.type = "submit";
+    savebutton.className = "save"
+    savebutton.textContent = "save";
+
+    main.appendChild(savebutton)
+
+    savebutton.addEventListener('click', ()=>{
+        const updatedvalue = nextinput.value
+        array[index] = updatedvalue
+
+        localStorage.setItem("keys", JSON.stringify(array));
+        display();
+
+        model.style.opacity = 0
+        main.style.display = "none"
+        model.style.display = "none"
+    })
+
+
 }
 display()
